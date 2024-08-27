031fbfb04740dc072f18c89fb942b596bc133efb
pedrovictormotasilva <motasilvapedrovictor@gmail.com>
2024-08-27 15:07:32 -0300

    adicionando testes de ponta a ponta para excesões de emails invalidos

--- src/test/java/com/ky/demo_park_api/UsuarioIT.java
+++ src/test/java/com/ky/demo_park_api/UsuarioIT.java
@@ -10,11 +10,13 @@ import org.springframework.test.web.reactive.server.WebTestClient;
 
 import com.ky.demo_park_api.web.dto.UserCreateDto;
 import com.ky.demo_park_api.web.dto.UserResponseDto;
+import com.ky.demo_park_api.web.exception.ErrorMessager;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Sql(scripts = "/sql/usuarios/usuarios-insert.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 @Sql(scripts = "/sql/usuarios/usuarios-delete.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 public class UsuarioIT {
+
 
     @Autowired
     WebTestClient testClient;
@@ -25,14 +27,53 @@ public class UsuarioIT {
                 .post()
                 .uri("/users")
                 .contentType(MediaType.APPLICATION_JSON)
-                .bodyValue(new UserCreateDto("joe@gmail.com", "12345678"))
+                .bodyValue(new UserCreateDto("oliver2clara@gmail.com", "12345678"))
                 .exchange()
                 .expectStatus().isCreated()
                 .expectBody(UserResponseDto.class)
                 .returnResult().getResponseBody();
         assertThat(responseBody).isNotNull();
-        assertThat(responseBody.getEmail()).isEqualTo("joe@gmail.com");
+        assertThat(responseBody.getEmail()).isEqualTo("oliver2clara@gmail.com");
         assertThat(responseBody.getRole()).isEqualTo("CLIENTE");
+    }
+
+    @Test
+    public void createUser_Emailnvalid_ReturnErrorMessage422() {
+        ErrorMessager responseBody = testClient
+                .post()
+                .uri("/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new UserCreateDto("", "12345678"))
+                .exchange()
+                .expectStatus().isEqualTo(422)
+                .expectBody(ErrorMessager.class)
+                .returnResult().getResponseBody();
+        assertThat(responseBody).isNotNull();
+        assertThat(responseBody.getStatus()).isEqualTo(422);
+
+        responseBody = testClient
+                .post()
+                .uri("/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new UserCreateDto("kayo@", "12345678"))
+                .exchange()
+                .expectStatus().isEqualTo(422)
+                .expectBody(ErrorMessager.class)
+                .returnResult().getResponseBody();
+        assertThat(responseBody).isNotNull();
+        assertThat(responseBody.getStatus()).isEqualTo(422);
+
+        responseBody = testClient
+                .post()
+                .uri("/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new UserCreateDto("kayo@gmail", "12345678"))
+                .exchange()
+                .expectStatus().isEqualTo(422)
+                .expectBody(ErrorMessager.class)
+                .returnResult().getResponseBody();
+        assertThat(responseBody).isNotNull();
+        assertThat(responseBody.getStatus()).isEqualTo(422);
 
     }
 }
--- src/test/resources/sql/usuarios/usuarios-insert.sql
+++ src/test/resources/sql/usuarios/usuarios-insert.sql
@@ -1,3 +1,3 @@
-INSERT INTO USUARIOS (id, email, password, role) VALUES (100, 'joe@gmail.com', '12345678', 'ROLE_CLIENTE');
-INSERT INTO USUARIOS (id, email, password, role) VALUES (101, 'kayo2@gmail.com', '12345678', 'ROLE_CLIENTE');
+INSERT INTO USUARIOS (id, email, password, role) VALUES (100, 'oliver_clara@gmail.com', '12345678', 'ROLE_CLIENTE');
+INSERT INTO USUARIOS (id, email, password, role) VALUES (101, 'kayo@gmail.com', '12345678', 'ROLE_CLIENTE');
 INSERT INTO USUARIOS (id, email, password, role) VALUES (102, 'kayo3@gmail.com', '12345678', 'ROLE_ADMIN');
